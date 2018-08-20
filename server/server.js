8b3e323eb6f33f1e22b033d62da6e00df5349788
trujic1000 <aleksandar.trujic@live.com>
2018-08-19 20:10:29 -0500

    PATCH /todos/:id added

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -26,7 +27,7 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
   Todo.find().then(todos => {
-    res.send(JSON.stringify(todos, undefined, 2));
+    res.send({todos});
   }, err => {
     res.status(400).send(err);
   });
@@ -47,7 +48,7 @@ app.get('/todos/:id', (req, res) => {
       return res.status(404).send('todo not found');
     }
     // If found
-    res.send(JSON.stringify(todo, undefined, 2));
+    res.send({todo});
   })
   .catch(err => {
     res.status(400).send();
@@ -67,9 +68,37 @@ app.delete('/todos/:id', (req, res) => {
       if (!doc) {
         return res.status(404).send();
       }
-      res.status(200).send(JSON.stringify(doc, undefined, 2));
+      res.status(200).send({todo});
     })
     .catch(err => res.status(400).send());
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const { id } = req.params;
+  // "DESTRUCTURING" req.body, but only properties that could be changed by user
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({todo});
+    })
+    .catch(err => {
+      res.status(400).send();
+    })
 });
 
 app.listen(port, () => {

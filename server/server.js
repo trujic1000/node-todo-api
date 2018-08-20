f7856a8b6f0c1a2bf6fbbc0103d470f5de7c0646
trujic1000 <aleksandar.trujic@live.com>
2018-08-20 14:23:50 -0500

    POST /users route and token method added

--- server/server.js
+++ server/server.js
@@ -101,6 +101,22 @@ app.patch('/todos/:id', (req, res) => {
     })
 });
 
+// POST /users
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password']);
+  const user = new User(body);
+  
+
+  user.save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then(token => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch(err => res.status(400).send());
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });

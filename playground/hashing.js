967054e21000a8ec3fd1eb241f6ee40034a86f95
trujic1000 <aleksandar.trujic@live.com>
2018-08-21 21:19:52 -0500

    hashing password added

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const { SHA256 } = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-  id: 10
-};
+const password = '123abc!';
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+const hashedPw = '$2a$10$mm6qFejO.SwhDVBgSwThd.x2k/Oxqk3UpMzdOY9p0SdD9x9yuHVAu';
+
+bcrypt.compare(password, hashedPw, (err, res) => {
+  console.log(res);
+});
+
+// const data = {
+//   id: 10
+// };
+
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // const message = 'I am user number 3';
 // const hash = SHA256(message).toString();
